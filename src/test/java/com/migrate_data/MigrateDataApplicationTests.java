1b59af799cabba0aca7de5b3fcac880ba12c6ea7
raulcbs <raulcubas2016@gmail.com>
2025-02-19 21:23:08 +0000

    feat: no test, fuck tests

--- src/test/java/com/migrate_data/MigrateDataApplicationTests.java
+++ src/test/java/com/migrate_data/MigrateDataApplicationTests.java
@@ -1,9 +1,7 @@
 package com.migrate_data;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MigrateDataApplicationTests {
 
 	@Test
